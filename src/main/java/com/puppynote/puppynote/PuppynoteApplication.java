857038a07ba54451a5ff0b52c00b5a07d5a9e761
basiliok <paledot01@gmail.com>
2025-10-05 12:40:43 -0500

    add application.yml for database and server setup

--- src/main/java/com/puppynote/puppynote/PuppynoteApplication.java
+++ src/main/java/com/puppynote/puppynote/PuppynoteApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PuppynoteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PuppynoteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PuppynoteApplication.class, args);
+    }
 
 }
