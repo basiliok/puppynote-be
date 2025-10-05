9132962a3a056e4ddd967955676aee4ba9715087
basiliok <paledot01@gmail.com>
2025-10-05 10:58:01 -0500

    initial commit

---
+++ src/main/java/com/puppynote/puppynote/PuppynoteApplication.java
@@ -0,0 +1,13 @@
+package com.puppynote.puppynote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppynoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuppynoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/puppynote/puppynote/PuppynoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puppynote.puppynote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppynoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
